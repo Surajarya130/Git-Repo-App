961b76848dc705d82d5c6047e843e8e054215f35
Surajarya130 <you@example.com>
2021-09-08 15:33:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import SearchBar from './components/SearchBar';
+import {useState} from 'react';
+import Alert from './components/Alert';
+import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // Use state hook for Dark/Light mode and alert option
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (msg, type)=>{
+    setAlert({
+      msg:msg,
+      type:type
+    })
+  }
+
+  // Certain timeout function to auto hide alert bar
+  setTimeout(() => {
+    setAlert(null) 
+  }, 1000);
+
+  // Toggling style for dark/light mode
+  const toggleMode = ()=>{
+    if(mode === 'dark'){
+      setMode('light');
+      document.body.style.backgroundColor = 'white';  
+      showAlert("Light Mode has been enabled", "Success");    
+
+    }else{
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark Mode has been enabled", "Success")   
+    }
+ 
+  }
+  return(
+    <>
+    <Router>
+    <Navbar title="GitApp" mode={mode}  toggleMode = {toggleMode}  />
+    <Alert  alert={alert} />
+
+    <Switch>
+          <Route path="/about">
+            <About mode={mode} />
+          </Route>
+          
+          <Route path="/">
+              <SearchBar mode={mode} showAlert={showAlert} />
+          </Route>
+    </Switch>
+
+    </Router>
+    </>
   );
 }
 
